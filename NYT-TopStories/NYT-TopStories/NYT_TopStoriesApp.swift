aa765270f99601db894dfcd35b29f9de50237008
ali rahal <ali.rahal01@gmail.com>
2023-05-26 00:02:49 +0300

    create Xcode project

---
+++ NYT-TopStories/NYT-TopStories/NYT_TopStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYT_TopStoriesApp.swift
+//  NYT-TopStories
+//
+//  Created by ali rahal on 26/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NYT_TopStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
